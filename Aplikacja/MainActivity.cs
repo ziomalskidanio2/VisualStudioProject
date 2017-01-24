d0d32b4cf37948675467cb22cb0f615eca6a817b
DanielDudkowski <danieldudkowski360@gmail.com>
2017-01-24 10:26:22 +0100

    Add project files.

---
+++ Aplikacja/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Aplikacja
+{
+    [Activity(Label = "Aplikacja", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
